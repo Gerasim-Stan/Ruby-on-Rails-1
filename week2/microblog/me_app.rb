76004bc5e3c8ba4c18ed0273c69392ae6d67d74e
Gerasim-Stan <gerasim.stanchev@gmail.com>
2015-04-09 15:20:04 +0300

    Edit routing to comply with reqs and add #show view

--- week2/microblog/me_app.rb
+++ week2/microblog/me_app.rb
@@ -1,12 +1,16 @@
 require 'sinatra'
 require 'erubis'
+require 'sinatra/contrib'
+require 'sinatra/base'
 set :erb, :escape_html => true
+set :method_override, true
+use Rack::MethodOverride
 
 class Post
   attr_accessor :title, :body
 
-  def initialize(title, body)
-    @title = title if (1..40).include?  title.length
+  def initialize(title: nil, body: nil)
+    @title = title if title.length > 1
     @body  = body  if (1..256).include? body.length
   end
 
@@ -14,27 +18,35 @@ class Post
 end
 
 ids = 0
-params = {}
-params[(ids += 1).to_s] = Post.new('Wee', 'Amazing blog post! Beware, peasants!')
-p params
+posts = {}
+posts[(ids += 1).to_s] = Post.new(title: 'Wee', body: 'Amazing blog post! Beware, peasants!')
+p posts
+
 get '/' do
-  erb :index, :locals => {:params => params}
+  erb :index, :locals => {:posts => posts}
 end
 
 get '/new' do
-  erb :new, :locals => {:params => params, :post_id => (ids += 1)}
+  erb :new, :locals => {:posts => posts}
 end
 
-post '/posts/:post_id' do
-  "#{request.POST}"
+post '/new' do
+  posts[(posts.keys.sort.last.to_i + 1).to_s] = Post.new(title: request.POST['title'].strip, body: request.POST['body'].strip)
+  redirect '/posts'
 end
 
-get '/posts/:id' do |id|
-  return "There's no post with #{id} for id present, m'lady *tips fedora*." if params[id].nil?
-  "#{params[id].title}\n #{params[id].body}"
+get '/:id' do |id|
+  return redirect '/' if id == 'posts'
+  return "There's no post with #{id} for id present, m'lady *tips fedora*." if posts[id].nil?
+  erb :show, :locals => {:post => posts[id.to_s], id: id}
 end
 
 get '/posts' do
-  @post = Post
-  'Yaay'
+  erb :index, :locals => {:posts => posts}
+end
+
+delete '/:id' do |id|
+  p 'defklopgrjmbhktjmb;plobhjmrpo[r0o-4it-0fkm;;;;;;smmm;dsklgvjmeopjkoejriteopfrlgijeogtjuleko'
+  posts.delete(id)
+  redirect '/'
 end
